795a95c272146280c77cbef9b0a82640ab2576aa
jPerpetuo <joao.ariel@crearenet.com.br>
2026-08-05 14:28:06 -0300

    Add initial plugin scaffold

---
+++ lang/pt_br/local_courseqbankcopy.php
@@ -0,0 +1,12 @@
+<?php
+// This file is part of Moodle - https://moodle.org/.
+
+$string['pluginname'] = 'Cópia independente de bancos de questões';
+$string['copyquestions'] = 'Copiar bancos de questões para este curso';
+$string['copyquestions_desc'] = 'Cria cópias independentes dos bancos de questões do curso de origem durante a importação.';
+$string['modecopy'] = 'Copiar bancos de questões para este curso';
+$string['modereuse'] = 'Reutilizar bancos de questões existentes';
+$string['allowreuseselection'] = 'Permitir que usuários autorizados escolham o modo de reutilização';
+$string['allowreuseselection_desc'] = 'Usuários com a capability local/courseqbankcopy:choosereusemode poderão selecionar a reutilização durante a importação.';
+$string['courseqbankcopy:choosereusemode'] = 'Escolher o modo de reutilização de bancos de questões durante a importação de curso';
+$string['privacy:metadata'] = 'O plugin Cópia independente de bancos de questões não armazena dados pessoais.';
