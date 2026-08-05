b44ffb941bb3d3045db24bcc1b0530478cae0ae1
jPerpetuo <joao.ariel@crearenet.com.br>
2026-08-05 14:27:34 -0300

    Add initial plugin scaffold

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+// This file is part of Moodle - https://moodle.org/.
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_courseqbankcopy';
+$plugin->version = 2026080501;
+$plugin->requires = 2025100601;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0-alpha';
