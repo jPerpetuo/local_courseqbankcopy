58d6d4b297227eafee8226d2ce6493eb258c6db1
jPerpetuo <joao.ariel@crearenet.com.br>
2026-08-13 15:29:32 -0300

    Load admin library in diagnostics page

--- diagnostics.php
+++ diagnostics.php
@@ -23,6 +23,7 @@
  */
 
 require_once(__DIR__ . '/../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
 
 use local_courseqbankcopy\local\diagnostic_report;
 
--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_courseqbankcopy';
-$plugin->version = 2026081301;
+$plugin->version = 2026081302;
 $plugin->requires = 2025100601;
 $plugin->supported = [501, 502];
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.2.14-alpha';
+$plugin->release = '0.2.15-alpha';
