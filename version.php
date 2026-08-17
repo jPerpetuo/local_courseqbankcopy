0c3ed8c8279ed945ae5526dbdeea4678d71c4a43
jPerpetuo <joao.ariel@crearenet.com.br>
2026-08-17 15:01:39 -0300

    Prepara versão 0.3.0-rc1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_courseqbankcopy';
-$plugin->version = 2026081401;
+$plugin->version = 2026081702;
 $plugin->requires = 2025100601;
 $plugin->supported = [501, 502];
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.2.17-alpha';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '0.3.0-rc1';
